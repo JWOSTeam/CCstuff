511c8caccae1730fe2c6af0eeea78999a481c7c7
JT_ <1steveminecraft1@gmail.com>
2024-07-09 07:47:39 -0500

    Add files via upload

---
+++ generate_staff_card.lua
@@ -0,0 +1,35 @@
+-- generate_staff_card.lua
+local diskSide = "right"  -- Adjust based on where your disk drive is located
+local ticketIDFile = "ticket_id.txt"
+
+local function writeStaffIDToDisk()
+    local mountPath = disk.getMountPath(diskSide)
+    if mountPath then
+        local file = fs.open(fs.combine(mountPath, ticketIDFile), "w")
+        file.write("420")
+        file.close()
+        print("Staff ID 420 written to disk.")
+    else
+        print("No disk present on " .. diskSide)
+    end
+end
+
+local function main()
+    term.clear()
+    term.setCursorPos(1, 1)
+    print("Please insert a disk to receive the staff ID.")
+
+    -- Wait until a disk is inserted
+    os.pullEvent("disk")
+
+    writeStaffIDToDisk()
+    disk.eject(diskSide)
+    print("Disk ejected with staff ID 420.")
+
+    -- Wait until the disk is removed before ending the program
+    while disk.isPresent(diskSide) do
+        sleep(1)  -- Check every second
+    end
+end
+
+main()
