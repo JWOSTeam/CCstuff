1b45943c55af5aaa0072953ae7c57f505b67e256
JT_ <1steveminecraft1@gmail.com>
2024-07-10 20:12:11 -0500

    Update Catalog.lua

--- Catalog.lua
+++ Catalog.lua
@@ -1,17 +1,18 @@
 -- Client Program
 
-local serverID = 22  -- Set the ID of the master server
+local serverID = 1  -- Set the ID of the master server
+local rednetSide = "top"  -- Set the side for the modem
 local itemList = {}
 
 -- Function to request item list from the server
 local function requestItemList()
     rednet.send(serverID, "REQUEST_ITEM_LIST")
-    local id, message = rednet.receive(10)  -- Wait for 10 seconds for a response
+    local id, message = rednet.receive(5)  -- Wait for 5 seconds for a response
     if id == serverID and type(message) == "table" then
         itemList = message
         return true
     else
-        print("Request timeout - unable to retrieve the item list. The master server may be down or disconnected. Terminating.")
+        print("Failed to retrieve item list.")
         itemList = {}
         return false
     end
@@ -36,11 +37,11 @@ local function placeOrder()
     print("Enter your name/alias:")
     local customerName = read()
 
-    print("Enter the name of the item you want to order (case sensitive):")
+    print("Enter the name of the item you want to order:")
     local itemName = read()
 
     if not itemList[itemName] then
-        print("Item not found. Run the program again to try again.")
+        print("Item not found.")
         return
     end
 
@@ -63,14 +64,14 @@ end
 
 -- Main program loop
 local function main()
-    rednet.open("top")
+    rednet.open(rednetSide)
     print("Requesting item list from the server...")
 
     if requestItemList() and displayItems() then
         placeOrder()
     end
 
-    rednet.close("top")
+    rednet.close(rednetSide)
 end
 
 main()
